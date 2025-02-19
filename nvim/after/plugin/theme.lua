f465ce8b6caadb44b8ec7e9930eef7e3241efe7b
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2025-02-19 13:14:52 -0300

    fix transparency override

--- nvim/after/plugin/theme.lua
+++ nvim/after/plugin/theme.lua
@@ -1,11 +1,14 @@
 local ok, github_theme = pcall(require, 'github-theme')
-if not ok then
-	return
+if ok then
+    github_theme.setup({
+        -- ...
+    })
 end
 
-github_theme.setup({
-   --...
-})
+-- Set colorscheme
+local color = "github_dark_default"
+vim.cmd("colorscheme " .. color)
 
-vim.cmd('colorscheme github_dark_default')
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
