c56cdf8e4a4a689632d183b069de3b29e9c17370
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2025-06-25 17:24:19 -0400

    add ngrok install script (#5)

---
+++ scripts/install_ngrok.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if command -v ngrok &> /dev/null ; then
+  echo "ngrok is already installed. Skipping installation."
+else
+  echo "Installing ngrok..."
+    curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+  && sudo apt update \
+  && sudo apt install ngrok
+fi
+
+
